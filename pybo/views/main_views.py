01f3ab18eaf6daa146551152780e705b96962770
임현규 <ihg1130@naver.com>
2023-09-15 01:25:39 +0900

    오류 화면 기능

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint, url_for, render_template, flash, request, session, g, Flask, redirect, current_app
+from flask import Flask, Blueprint, current_app, url_for, render_template, flash, request, session, g, Flask, redirect, current_app
 from pybo.models import Question
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
@@ -19,11 +19,7 @@ from pybo.forms import UserCreateForm, UserLoginForm, QuestionForm
 from pybo.models import User, Question
 
 import functools
-import config
 
-# from config1 import CLIENT_ID, REDIRECT_URI
-# from controller import Oauth
-# from model import UserModel, UserData
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
@@ -134,6 +130,7 @@ class Oauth:
 
 @bp.route('/')
 def index():
+      current_app.logger.info("INFO 레벨로 출력")
       return render_template("index.html")
 
 @bp.route('/memorial')
@@ -144,122 +141,7 @@ def memorial():
 def index1():
       return redirect(url_for('question._list'))
 
-# @bp.route('/')
-# def home():
-#     return render_template("APIExamNaverLogin.html")
- 
 @bp.route('/naverlogin')
 def login():
     return render_template("callback.html")
-
-# @bp.route('/3')
-# def home():
-#     return render_template("kakao.html")
-
-
-
-# @bp.route("/oauth")
-# def oauth_api():
-#     create_app()
-# #     """
-# #     # OAuth API [GET]
-# #     사용자로부터 authorization code를 인자로 받은 후,
-# #     아래의 과정 수행함
-# #     1. 전달받은 authorization code를 통해서
-# #         access_token, refresh_token을 발급.
-# #     2. access_token을 이용해서, Kakao에서 사용자 식별 정보 획득
-# #     3. 해당 식별 정보를 서비스 DB에 저장 (회원가입)
-# #     3-1. 만약 이미 있을 경우, (3) 과정 스킵
-# #     4. 사용자 식별 id를 바탕으로 서비스 전용 access_token 생성
-# #     """
-#     code = str(request.args.get('code'))
-    
-#     oauth = Oauth()
-#     auth_info = oauth.auth(code)
-#     user = oauth.userinfo("Bearer " + auth_info['access_token'])
-    
-#     user = UserData(user)
-#     UserModel().upsert_user(user)
-
-
-#     resp = make_response(render_template('index.html'))
-#     access_token = create_access_token(identity=user.id)
-#     refresh_token = create_refresh_token(identity=user.id)
-#     resp.set_cookie("logined", "true")
-#     set_access_cookies(resp, access_token)
-#     set_refresh_cookies(resp, refresh_token)
-
-#     return resp
-
-
-# @bp.route('/token/refresh')
-# @jwt_required
-# def token_refresh_api():
-#     """
-#     Refresh Token을 이용한 Access Token 재발급
-#     """
-#     user_id = get_jwt_identity()
-#     resp = jsonify({'result': True})
-#     access_token = create_access_token(identity=user_id)
-#     set_access_cookies(resp, access_token)
-#     return resp
-
-
-# @bp.route('/token/remove')
-# def token_remove_api():
-#     """
-#     Cookie에 등록된 Token 제거
-#     """
-#     resp = jsonify({'result': True})
-#     unset_jwt_cookies(resp)
-#     resp.delete_cookie('logined')
-#     return resp
-
-
-# # @bp.route("/userinfo")
-# # @jwt_required
-# # def userinfo():
-# #      """
-# #      Access Token을 이용한 DB에 저장된 사용자 정보 가져오기
-# #      """
-# #      user_id = get_jwt_identity()
-# #      userinfo = UserModel().get_user(user_id).serialize()
-# #      return jsonify(userinfo)
-
-
-# @bp.route('/oauth/url')
-# def oauth_url_api():
-#     """
-#     Kakao OAuth URL 가져오기
-#     """
-#     return jsonify(
-#         kakao_oauth_url="https://kauth.kakao.com/oauth/authorize?client_id=%s&redirect_uri=%s&response_type=code" \
-#         % ("6925e2e950e402e6d1d144af81ee1292","http://localhost:5000/oauth")
-#     )
-
-
-# @bp.route("/oauth/refresh", methods=['POST'])
-# def oauth_refesh_api():
-#     """
-#     # OAuth Refresh API
-#     refresh token을 인자로 받은 후,
-#     kakao에서 access_token 및 refresh_token을 재발급.
-#     (% refresh token의 경우, 
-#     유효기간이 1달 이상일 경우 결과에서 제외됨)
-#     """
-#     refresh_token = request.get_json()['refresh_token']
-#     result = Oauth().refresh(refresh_token)
-#     return jsonify(result)
-
-
-# @bp.route("/oauth/userinfo", methods=['POST'])
-# def oauth_userinfo_api():
-#     """
-#     # OAuth Userinfo API
-#     kakao access token을 인자로 받은 후,
-#     kakao에서 해당 유저의 실제 Userinfo를 가져옴
-#     """
-#     access_token = request.get_json()['access_token']
-#     result = Oauth().userinfo("Bearer " + access_token)
-#     return jsonify(result)
 
