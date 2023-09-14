e774dcb23dbd89c5e7e74f814b90392b5b3e3111
임현규 <ihg1130@naver.com>
2023-09-15 00:18:45 +0900

    404 오류등록

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
@@ -25,6 +25,8 @@ naming_convention = {
 db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
 
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 def create_app():
     app = Flask(__name__)
@@ -37,6 +39,8 @@ def create_app():
     app.config['JWT_REFRESH_TOKEN_EXPIRES'] = 100
     jwt = JWTManager(app)
    
+   # 오류페이지
+    app.register_error_handler(404, page_not_found)
 
     # ORM
     db.init_app(app)
